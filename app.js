abc97736cce6ac9be7ce619be91a43bbd42259fb
zhangsq <zhangsq@outlook.com>
2014-03-30 23:48:27 +0800

    NodeJS静态服务器
    一个练习的做品，可以做为测试静态页面的服务器

---
+++ app.js
@@ -0,0 +1,94 @@
+var http = require('http');
+var zlib = require('zlib');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+
+var config = require('./config'),
+	serverSetting = config.serverSetting,
+	mimeType = config.mimeType,
+	expires = config.expires,
+	compress = config.compress;
+
+function pathHandle (request, response, realPath, pathname) {
+	fs.stat(realPath, function (err, stats) {
+		if (err) {
+			//文件不存在时返回404
+			response.writeHead(404, 'Not Found', {'Content-Type' : mimeType['txt']});			
+			response.write('This request URL ' + pathname + ' was not found on this server');
+			response.end();
+		} else {
+			if (stats.isDirectory()) {
+				//如果路径是文件夹自动加上默认文件
+				realPath = path.join(realPath, '/', serverSetting.defaultFile);
+				pathHandle(realPath);
+			} else {
+				//根据扩展名确定Content-Type
+				var ext = path.extname(realPath);
+				ext = ext ? ext.slice(1) : 'unknown';
+				var contentType = mimeType[ext] || 'text/plain';
+				response.setHeader('Content-Type', contentType);
+
+				//设置返回头中文件修改时间
+				var lastModified = stats.mtime.toUTCString();
+				var ifModifiedSince = 'If-Modified-Since'.toLowerCase();
+				response.setHeader('Last-Modified', lastModified);
+
+				if (ext.match(expires.fileMatch)) {
+					//返回头中设置过期时间
+					var date = new Date();
+					date.setTime(date.getTime() + expires.maxAge);
+					response.setHeader('Expires', date.toUTCString());
+					response.setHeader('Cache-Control', 'max-age=' + expires.maxAge);
+				}
+
+				if (request.headers[ifModifiedSince] && lastModified == request.headers[ifModifiedSince]) {
+					//如果文件未过期返回304
+					response.writeHead(304, 'Not Modified');
+					response.end();
+				} else {
+					var raw = fs.createReadStream(realPath);
+					var acceptEncoding = request.headers['accept-encoding'] || '';
+					var matched = ext.match(compress.match);
+
+					//判断是否启用GZip
+					if (matched && acceptEncoding.match(/\bgzip\b/)) {
+						response.writeHead(200, 'OK', {'Content-Encoding' : 'gzip'});
+						raw.pipe(zlib.createGzip()).pipe(response);
+					} else if (matched && acceptEncoding.match(/\bdeflate\b/)) {
+						response.writeHead(200, 'OK', {'Content-Encoding' : 'deflate'});
+						raw.pipe(zlib.createDeflate()).pipe(response);
+					} else {
+						response.writeHead(200, 'OK');
+						raw.pipe(response);
+					}
+				}
+			}
+		}
+	});
+}
+
+var server = http.createServer(function (request, response) {
+	var pathname = url.parse(request.url).pathname;
+	if (pathname.slice(-1) === '/') {
+		pathname = pathname + serverSetting.defaultFile;
+	}
+	var realPath = path.join(serverSetting.rootPath, path.normalize(pathname.replace(/\.\./g, '')));
+	
+
+	fs.exists(realPath, function (exists) {
+		if (!exists) {
+			response.writeHead(404, {'Content-Type' : 'text/plain'});
+			response.write('This request URL ' + pathname + ' was not found on this server.');
+			response.end();
+		} else {
+			var ext = path.extname(realPath);
+			ext = ext ? ext.slice(1) : 'unknown';
+			response.setHeader('Content-Type', mimeType[ext] || 'unknown');
+			
+			pathHandle(request, response, realPath, pathname);
+		}
+	});
+});
+server.listen(serverSetting.port);
+console.log('Server runing at port: ' + serverSetting.port);
---
+++ config.js
@@ -0,0 +1,42 @@
+//服务器基本设置
+exports.serverSetting = {
+	//端口
+	"port" : 8000,
+	//根目录
+	"rootPath" : "./assets",
+	//是否启用GZip
+	"gzip" : true
+};
+
+//mimeType设置
+exports.mimeType = {
+	"css" 	: "text/css",
+	"gif" 	: "image/gif",
+	"html" 	: "text/html",
+	"ico" 	: "image/x-icon",
+	"jpeg" 	: "image/jpeg",
+	"jpg" 	: "image/jpeg",
+	"js" 	: "text/javascript",
+	"json" 	: "application/json",
+	"pdf" 	: "application/pdf",
+	"png" 	: "image/png",
+	"svg" 	: "image/svg+xml",
+	"swf" 	: "application/x-shockwave-flash",
+	"tiff" 	: "image/tiff",
+	"txt" 	: "text/plain",
+	"wav" 	: "audio/x-wav",
+	"wma" 	: "audio/x-ms-wma",
+	"wmv" 	: "video/x-ms-wmv",
+	"xml" 	: "text/xml"
+};
+
+//缓存设置
+exports.expires = {
+	fileMatch : /^(gif|png|jpg|js|css)$/ig,
+	maxAge : 606024365
+};
+
+//GZip压缩选项
+exports.compress = {
+	match : /css|js|html/ig
+};
